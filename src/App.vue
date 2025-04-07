35adc937469002f71f324e3f217721409ec13152
Maggie <641523598@qq.com>
2025-04-07 08:47:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-red-400 form">
+    我是谁
+    <span>哈哈哈哈
+      <h1>的他</h1>
+    </span>
+  </div>
+</template>
+
+<style scoped lang="css">
+.form {
+  span {
+    color: blue;
+
+    h1 {
+      color: yellow
+    }
+  }
+}
+</style>
