591b10c01e607f3b2108f2176d6faadb443ca25e
Maggie <641523598@qq.com>
2025-04-07 10:11:59 +0800

    环境变量

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-createApp(App).mount("#app");
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+console.log(import.meta.env.VITE_ENV)
+createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      "@": "./src/*",
-    },
+      '@': './src/*'
+    }
   },
-});
+  build: {
+    outDir: 'dist', // 输出目录
+    assetsDir: 'assets', // 静态资源目录
+    sourcemap: true, // 生成源映射文件
+    minify: 'terser', // 使用 Terser 进行压缩
+    rollupOptions: {
+      input: {
+        main: 'index.html' // 主入口文件
+        // 可以添加更多入口文件
+      },
+      output: {
+        chunkFileNames: 'chunks/[name]-[hash].js', // 输出 chunk 文件命名
+        assetFileNames: 'assets/[name]-[hash][extname]' // 静态资源命名
+      }
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // 代理目标地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
